ecbd602e13db4fb73d016ce516ac01fd09d0c1b7
aziz42_ux <azizisoffline@gmail.com>
2021-10-22 14:21:49 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const redux = require("redux");
+  const createStore = redux.createStore;
+  const Buy_Books = "Buy_Books";
+  const Buy_Pens ="Buy_Pens";
+  const initialState = {
+    numberOfBooks: 10,
+    numberOfPens : 5,
+  };
+  function BuyBooks() {
+    return {
+      type: Buy_Books,
+      info: "First Redux Code",
+    };
+  }
+  function BuyPens() {
+    return {
+      type: Buy_Pens,
+      info: "First Redux Code Pen",
+    };
+  }
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case "Buy_Books":
+        return {
+          ...state,
+          numberOfBooks: state.numberOfBooks - 1,
+        };
+        case "Buy_Pens" : return{
+          ...state,
+          numberOfPens : state.numberOfPens-1
+        }
+      default:
+        return state;
+    }
+  };
+  const store = createStore(reducer);
+  console.log("initial state value", store.getState());
+  const unsubscribe = store.subscribe(() => {
+    console.log("state value updated ", store.getState());
+  });
+  store.dispatch(BuyBooks());
+  store.dispatch(BuyPens());
+  unsubscribe();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Redux App</h1>
     </div>
   );
 }
